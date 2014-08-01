db41fa8328dd95016b00fee1f77fec1b1af122b9
patrickd2015 <patrickd2015@gmail.com>
2014-07-31 22:24:12 -0700

    first commit

---
+++ Johri Stories/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Johri Stories
+//
+//  Created by Patrick Dong on 7/31/14.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Johri StoriesTests/Johri_StoriesTests.m
@@ -0,0 +1,34 @@
+//
+//  Johri_StoriesTests.m
+//  Johri StoriesTests
+//
+//  Created by Patrick Dong on 7/31/14.
+//
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Johri_StoriesTests : XCTestCase
+
+@end
+
+@implementation Johri_StoriesTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
